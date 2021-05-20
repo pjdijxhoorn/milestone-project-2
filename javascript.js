5700fd07252b6ee44170fd2b6799ad61c4f4dac9
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-05-20 14:57:58 +0200

    added styling to buttons and  made a function to put array players in a table.

--- javascript.js
+++ javascript.js
@@ -6,17 +6,45 @@ function addPlayer() {
     players.push(add.value);
     console.log(players)
     document.getElementById("table").innerHTML=players.toString();
+    document.getElementById("error").innerHTML=``
 }
 
 function RemovePlayer(){
-    let usernameRemove =document.getElementById("nameRem")
-    if( players.includes(usernameRemove.value)){
-   let number = players.indexOf(usernameRemove.value)
+    let Remove =document.getElementById("nameRem")
+    if( players.includes(Remove.value)){
+   let number = players.indexOf(Remove.value)
     players.splice(number, 1)
     document.getElementById("table").innerHTML=players.toString();
+        document.getElementById("error").innerHTML=``
     }else{
-        document.getElementById("error").innerHTML=`this person is not on the list!`;
+        document.getElementById("error").innerHTML=`This person is not on the list!`;
     }}
 
+function buildTable(){
 
+    let html = `
+  <table>
+    <thead>
+      <tr>
+        <th>Players:</th>
+      </tr>
+    </thead>
+    <tbody>
+`;
 
+    for (let player of players) {
+        let rowHtml = `
+    <tr>
+      <td>${player}</td>
+    </tr>
+  `;
+        html += rowHtml;
+    }
+    html += `
+  </tbody>
+</table>
+`;
+    return html;
+}
+let table= buildTable();
+document.getElementById("table").innerHTML= table;
