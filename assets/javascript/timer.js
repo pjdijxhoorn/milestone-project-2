6e6a8c9789d238df95095cbf089496f20b25d06e
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-05-28 13:58:17 +0200

    fixed some text. added draggable blocks based on a array.

---
+++ assets/javascript/timer.js
@@ -0,0 +1,96 @@
+let spelers = ["Tim", "harry"];
+
+function mediaWidth(x){
+    if(x.matches){
+        console.log("test")
+        createDivs();
+    }
+}
+let x =window.matchMedia("(min-width: 700px)")
+mediaWidth(x);
+x.addListener(mediaWidth)
+
+
+function createDivs(){
+    for (let speler in spelers){
+        let newElement =document.createElement('div');
+        newElement.className="item one d-none d-sm-block d-md-block;"
+        newElement.innerHTML=`name<br>00:00<br>00:00`;
+        document.getElementById("container").appendChild(newElement);
+    }
+}
+
+let container = document.querySelector("#container");
+let activeItem = null;
+
+let active = false;
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+function dragStart(e) {
+
+    if (e.target !== e.currentTarget) {
+        active = true;
+
+        // this is the item we are interacting with
+        activeItem = e.target;
+
+        if (activeItem !== null) {
+            if (!activeItem.xOffset) {
+                activeItem.xOffset = 0;
+            }
+
+            if (!activeItem.yOffset) {
+                activeItem.yOffset = 0;
+            }
+
+            if (e.type === "touchstart") {
+                activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
+                activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
+            } else {
+                console.log("doing something!");
+                activeItem.initialX = e.clientX - activeItem.xOffset;
+                activeItem.initialY = e.clientY - activeItem.yOffset;
+            }
+        }
+    }
+}
+
+function dragEnd(e) {
+    if (activeItem !== null) {
+        activeItem.initialX = activeItem.currentX;
+        activeItem.initialY = activeItem.currentY;
+    }
+
+    active = false;
+    activeItem = null;
+}
+
+function drag(e) {
+    if (active) {
+        if (e.type === "touchmove") {
+            e.preventDefault();
+
+            activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
+            activeItem.currentY = e.touches[0].clientY - activeItem.initialY;
+        } else {
+            activeItem.currentX = e.clientX - activeItem.initialX;
+            activeItem.currentY = e.clientY - activeItem.initialY;
+        }
+
+        activeItem.xOffset = activeItem.currentX;
+        activeItem.yOffset = activeItem.currentY;
+
+        setTranslate(activeItem.currentX, activeItem.currentY, activeItem);
+    }
+}
+
+function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
