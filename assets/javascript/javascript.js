9307bf06b102a5a525291483a727f57e1207fadd
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-06-04 10:37:58 +0200

    added eventlistners to all the toggle switches. added names to the timer page players.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,20 +10,21 @@ function homeButton() {
     $(".content-area-results").hide();
     $(".content-area-home").show();
     $("#watchGroup").hide();
-    $("#btngroup").hide();
+    $(".btngroup").hide();
     $(".img-block").hide();
 }
 
 function timerButton() {
     $(".content-area-timer").show();
     $(".img-block").show();
-    $("#btngroup").show();
+    $(".btngroup").show();
     $("#watchGroup").show();
     $(".content-area-results").hide();
     $(".content-area-home").hide();
 
     $(".item").remove();
     createDivs();
+    addNames();
 
 
 }
@@ -33,7 +34,7 @@ function resultsButton() {
     $(".content-area-results").show();
     $(".content-area-home").hide();
     $("#watchGroup").hide();
-    $("#btngroup").hide();
+    $(".btngroup").hide();
     $(".img-block").hide();
 }
 
@@ -90,45 +91,272 @@ function buildTable() {
 }
 
 
-// the code below is for the toggle buttons to switch between timers
-addNames();
-function addNames(){
+// the code below is to add names to the timer page
+function addNames() {
     let spans = document.querySelectorAll('div[class="name"]');
-
-    for (i = 0; i < players.length; i++) {
+    for (let i = 0; i < players.length; i++) {
         spans[i].innerHTML = players[i];
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    let checkbox = document.querySelector('input[type="checkbox"]');
-    let start = document.querySelectorAll("a[href='#start']")
-    let stop = document.querySelectorAll("a[href='#stop']")
-    let reset = document.querySelectorAll("a[href='#reset']")
-    checkbox.addEventListener('change', function () {
-        if (checkbox.checked && trueCheck === true) {
-            // do this
-            console.log('Checked');
-            start[1].click();
-            stop[0].click();
-            reset[0].click();
+// the code below is for the toggle buttons to switch between timers
 
-        }else if(trueCheck === true){
-            // do that
-            console.log('Not checked');
-            start[0].click();
-            stop[1].click();
-        }else{
-            console.log("start is not on")
-        }
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let checkbox = document.querySelectorAll('input[type="checkbox"]');
+    let start = document.querySelectorAll("a[href='#start']");
+    let stop = document.querySelectorAll("a[href='#stop']");
+    let reset = document.querySelectorAll("a[href='#reset']");
+
+        checkbox[0].addEventListener('change', function check1() {
+
+
+            if (checkbox[0].checked && trueCheck === true) {
+                start[1].click();
+                stop[0].click();
+                reset[0].click();
+
+            } else if (trueCheck === true) {
+                start[0].click();
+                stop[1].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[1].addEventListener('change', function () {
+
+            if (checkbox[1].checked && trueCheck === true) {
+                start[3].click();
+                stop[2].click();
+                reset[2].click();
+
+            } else if (trueCheck === true) {
+                start[2].click();
+                stop[3].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[2].addEventListener('change', function () {
+
+            if (checkbox[2].checked && trueCheck === true) {
+                start[5].click();
+                stop[4].click();
+                reset[4].click();
+
+            } else if (trueCheck === true) {
+                start[4].click();
+                stop[5].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[3].addEventListener('change', function () {
+
+            if (checkbox[3].checked && trueCheck === true) {
+                start[7].click();
+                stop[6].click();
+                reset[6].click();
+
+            } else if (trueCheck === true) {
+                start[6].click();
+                stop[7].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[4].addEventListener('change', function () {
+
+            if (checkbox[4].checked && trueCheck === true) {
+                start[9].click();
+                stop[8].click();
+                reset[8].click();
+
+            } else if (trueCheck === true) {
+                start[8].click();
+                stop[9].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[5].addEventListener('change', function () {
+
+            if (checkbox[5].checked && trueCheck === true) {
+                start[11].click();
+                stop[10].click();
+                reset[10].click();
+
+            } else if (trueCheck === true) {
+                start[10].click();
+                stop[11].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[6].addEventListener('change', function () {
+
+            if (checkbox[6].checked && trueCheck === true) {
+                start[13].click();
+                stop[12].click();
+                reset[12].click();
+
+            } else if (trueCheck === true) {
+                start[12].click();
+                stop[13].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[7].addEventListener('change', function () {
+
+            if (checkbox[7].checked && trueCheck === true) {
+                start[15].click();
+                stop[14].click();
+                reset[14].click();
+
+            } else if (trueCheck === true) {
+                start[14].click();
+                stop[15].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[8].addEventListener('change', function () {
+
+            if (checkbox[8].checked && trueCheck === true) {
+                start[17].click();
+                stop[16].click();
+                reset[16].click();
+
+            } else if (trueCheck === true) {
+                start[16].click();
+                stop[17].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[9].addEventListener('change', function () {
+
+            if (checkbox[9].checked && trueCheck === true) {
+                start[19].click();
+                stop[18].click();
+                reset[18].click();
+
+            } else if (trueCheck === true) {
+                start[18].click();
+                stop[19].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[10].addEventListener('change', function () {
+
+            if (checkbox[10].checked && trueCheck === true) {
+                start[21].click();
+                stop[20].click();
+                reset[20].click();
+
+            } else if (trueCheck === true) {
+                start[20].click();
+                stop[21].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[11].addEventListener('change', function () {
+
+            if (checkbox[11].checked && trueCheck === true) {
+                start[23].click();
+                stop[22].click();
+                reset[22].click();
+
+            } else if (trueCheck === true) {
+                start[22].click();
+                stop[23].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[12].addEventListener('change', function () {
+
+            if (checkbox[12].checked && trueCheck === true) {
+                start[25].click();
+                stop[24].click();
+                reset[24].click();
+
+            } else if (trueCheck === true) {
+                start[24].click();
+                stop[25].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[13].addEventListener('change', function () {
+
+            if (checkbox[13].checked && trueCheck === true) {
+                start[27].click();
+                stop[26].click();
+                reset[26].click();
+
+            } else if (trueCheck === true) {
+                start[26].click();
+                stop[27].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[14].addEventListener('change', function () {
+
+            if (checkbox[14].checked && trueCheck === true) {
+                start[29].click();
+                stop[28].click();
+                reset[28].click();
+
+            } else if (trueCheck === true) {
+                start[28].click();
+                stop[29].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
+        checkbox[15].addEventListener('change', function () {
+
+            if (checkbox[15].checked && trueCheck === true) {
+                start[31].click();
+                stop[30].click();
+                reset[30].click();
+
+            } else if (trueCheck === true) {
+                start[30].click();
+                stop[31].click();
+
+            } else {
+                console.log("start is not on")
+            }
+        });
     });
-});
 
 
 // code under this comment is to build div for every player
 
-var Stopwatch = function (elem, options) {
-    var timer = createTimer(),
+let Stopwatch = function (elem, options) {
+    let timer = createTimer(),
         startButton = createButton("start", start),
         stopButton = createButton("stop", stop),
         resetButton = createButton("reset", reset),
@@ -155,7 +383,7 @@ var Stopwatch = function (elem, options) {
     }
 
     function createButton(action, handler) {
-        var a = document.createElement("a");
+        let a = document.createElement("a");
         a.href = "#" + action;
         a.innerHTML = action;
         a.addEventListener("click", function (event) {
@@ -190,10 +418,9 @@ var Stopwatch = function (elem, options) {
     }
 
     function render() {
-        var h = Math.floor(clock / (1000 * 60 * 60)) % 24;
-        var m = Math.floor(clock / (1000 * 60)) % 60;
-        var s = Math.floor(clock / 1000) % 60;
-        var ms = Math.floor(clock % 1000);
+        let h = Math.floor(clock / (1000 * 60 * 60)) % 24;
+        let m = Math.floor(clock / (1000 * 60)) % 60;
+        let s = Math.floor(clock / 1000) % 60;
 
         if (h < 10) {
             h = "0" + h;
@@ -210,7 +437,7 @@ var Stopwatch = function (elem, options) {
     }
 
     function delta() {
-        var now = Date.now(),
+        let now = Date.now(),
             d = now - offset;
 
         offset = now;
@@ -223,28 +450,29 @@ var Stopwatch = function (elem, options) {
 };
 
 
-var elems = document.getElementsByClassName("basic");
-for (var i = 0, len = elems.length; i < len; i++) {
+let elems = document.getElementsByClassName("basic");
+for (let i = 0, len = elems.length; i < len; i++) {
     new Stopwatch(elems[i]);
 }
 
 function startBtn() {
     trueCheck = true;
     let startBtn = document.querySelectorAll("a[href='#start']")
-    for (var i = 0; i < startBtn.length; i++)
+    for (let i = 0; i < startBtn.length; i++)
         startBtn[i].click();
+
 }
 
 function stopBtn() {
     trueCheck = false;
     let stopBtn = document.querySelectorAll("a[href='#stop']")
-    for (var i = 0; i < stopBtn.length; i++)
+    for (let i = 0; i < stopBtn.length; i++)
         stopBtn[i].click();
 }
 
 function resetBtn() {
     let resetBtn = document.querySelectorAll("a[href='#reset']")
-    for (var i = 0; i < resetBtn.length; i++)
+    for (let i = 0; i < resetBtn.length; i++)
         resetBtn[i].click();
 }
 
@@ -255,7 +483,7 @@ function createDivs() {
     for (let player in players) {
         let newElement = document.createElement('div');
         newElement.className = "item d-none d-sm-block d-md-block;"
-        newElement.innerHTML = `${players[player]}<br>00:00<br>00:00`;
+        newElement.innerHTML = `${players[player]}`;
         document.getElementById("container").appendChild(newElement);
     }
 }
@@ -302,7 +530,7 @@ function dragStart(e) {
     }
 }
 
-function dragEnd(e) {
+function dragEnd() {
     if (activeItem !== null) {
         activeItem.initialX = activeItem.currentX;
         activeItem.initialY = activeItem.currentY;
